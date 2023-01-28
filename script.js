7629c79f8e32513419bba1f7d5a77e1a56fc0048
llcain <llcain2012@gmail.com>
2023-01-27 22:16:27 -0600

    added script.js file

---
+++ script.js
@@ -0,0 +1,17 @@
+/* Grab hamburger and ul nav-lists */
+
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navList.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((link) =>
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navList.classList.remove("active");
+    })
+);
+
