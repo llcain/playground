d425dbdc80a9ce59bd37b056cfaffc1740b06e23
llcain <llcain2012@gmail.com>
2023-06-21 22:36:54 -0500

    Added function to hamburger menu

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector(".nav-menu");
+const navList = document.querySelector(".nav-list");
 
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navList.classList.toggle("active");
+  });
+  
+  document.querySelectorAll(".nav-link").forEach((link) =>
+    link.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navList.classList.remove("active");
+    })
+  );
+  
